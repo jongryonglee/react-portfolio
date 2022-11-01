5818e7d03d758359a37ae7c7a670e87294870901
Jongryong Lee <jongryonglee7@gmail.com>
2022-10-31 17:52:12 -0700

    Fixed router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Menu from "./component/Menu";
 import Home from "./component/Home";
 import About from "./component/About";
@@ -12,11 +12,13 @@ function App() {
   return (
     <BrowserRouter>
       <Menu />
+      <Switch>
       <Route exact path="/react-portfolio/" component={Home} />
       <Route path="/react-portfolio/about" component={About} />
       <Route path="/react-portfolio/skills" component={Skills} />
       <Route path="/react-portfolio/works" component={Works} />
       {/* <Route path="/react-portfolio/blogs" component={Blogs} /> */}
+      </Switch>
       <Contact />
     </BrowserRouter>
   );
